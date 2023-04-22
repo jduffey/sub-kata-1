0099446cf53ba1d85e38e7079e74bb6b921ca761
Jed Duffey <jedduffey@gmail.com>
2023-04-22 10:46:27 -0400

    Initial commit: set up npm project for Jest testing; currently we are just reading in the file and converting its lines to entries in an array

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+let contents;
+try {
+    contents = fs.readFileSync('./submarine_kata_input.txt', 'utf-8');
+} catch (e) {
+    console.error('Error when trying to read file:', e);
+}
+
+const instructions = contents.split("\n");
+
+console.log(instructions);

a90f613b2a8366f9a375728cc0cafcaac3d53816
Jed Duffey <jedduffey@gmail.com>
2023-04-24 08:59:28 -0400

    Create test for moveForward method; separate initialization tests from movement tests

--- submarine.js
+++ submarine.js
@@ -6,6 +6,8 @@ class Submarine {
 
     getHorizontalPosition = () => this.horizontalPos;
     getVerticalPosition = () => this.verticalPos;
+
+    moveForward = (disp) => this.horizontalPos += disp;
 }
 
 module.exports = Submarine;
--- submarine.test.js
+++ submarine.test.js
@@ -1,19 +1,32 @@
 const Submarine = require('./submarine');
 
 describe("Submarine", () => {
-    it("starts with horizontal position as 0", () => {
-        const sut = new Submarine();
+    describe("initialization tests", () => {
+        it("starts with horizontal position as 0", () => {
+            const sut = new Submarine();
 
-        const actual = sut.getHorizontalPosition();
+            const actual = sut.getHorizontalPosition();
 
-        expect(actual).toBe(0);
+            expect(actual).toBe(0);
+        });
+
+        it("starts with vertical position as 0", () => {
+            const sut = new Submarine();
+
+            const actual = sut.getVerticalPosition();
+
+            expect(actual).toBe(0);
+        });
     });
 
-    it("starts with vertical position as 0", () => {
-        const sut = new Submarine();
+    describe("horiztonal movement tests", () => {
+        it("increases horiztonal position when given forward command", () => {
+            const sut = new Submarine();
+            sut.moveForward(5);
 
-        const actual = sut.getVerticalPosition();
+            const actual = sut.getHorizontalPosition();
 
-        expect(actual).toBe(0);
+            expect(actual).toBe(5);
+        });
     });
 });
